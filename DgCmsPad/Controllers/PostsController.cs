9d49e7f09d0c9dbbb43fcaefd5a89624b55152ec
Husseiny10 <95244061+Husseiny10@users.noreply.github.com>
2022-09-14 21:20:27 +0300

    Add project files.

---
+++ DgCmsPad/Controllers/PostsController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DgCmsPad.Infrastructure;
+using DgCmsPad.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DgCmsPad.Controllers
+{
+    public class PostsController : Controller
+    {
+
+        private readonly DgCmsPadContext context;
+        public PostsController(DgCmsPadContext context)
+        {
+            this.context = context;
+
+        }
+
+      
+        public async Task<IActionResult> Index(int p=1)
+        {
+            int pageSize = 1;
+            var posts = context.Posts.OrderByDescending(x => x.Id).Skip((p - 1) * pageSize).Take(pageSize);
+            ViewBag.PageNumber = p;
+            ViewBag.PageRange = pageSize;
+            ViewBag.TotalPages = (int)Math.Ceiling((decimal)context.Posts.Count() / pageSize);
+            return View(await posts.ToListAsync());
+        }
+
+        //get/posts/term
+        public async Task<IActionResult> PostsByTerm(string categorySlug,int p = 1)
+        {
+            Term term =await context.Terms.Where(x => x.Slug == categorySlug).FirstOrDefaultAsync();
+            if (term == null) return RedirectToAction("Index");
+
+            int pageSize = 6;
+            var posts = context.Posts.OrderByDescending(x => x.Id).Where(x=>x.PostTypeId ==term.Id).Skip((p - 1) * pageSize).Take(pageSize);
+            ViewBag.PageNumber = p;
+            ViewBag.PageRange = pageSize;
+            ViewBag.TotalPages = (int)Math.Ceiling((decimal)context.Posts.Where(x => x.PostTypeId == term.Id).Count() / pageSize);
+            ViewBag.CategoryName = term.Name;
+            return View(await posts.ToListAsync());
+        }
+    }
+}
---
+++ DgCmsPad/Migrations/20220826103222_ThirdCreate.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DgCmsPad.Migrations
+{
+    public partial class ThirdCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Posts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: false),
+                    Slug = table.Column<string>(nullable: true),
+                    Details = table.Column<string>(nullable: false),
+                    Date = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    PostTypeId = table.Column<int>(nullable: false),
+                    Image = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Posts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Posts_Terms_PostTypeId",
+                        column: x => x.PostTypeId,
+                        principalTable: "Terms",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Posts_PostTypeId",
+                table: "Posts",
+                column: "PostTypeId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Posts");
+        }
+    }
+}
